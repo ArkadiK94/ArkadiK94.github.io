e2dc1482e0b82c5372ac94e457ee932a46ca41da
Arkadi Koifman <76536506+ArkadiK94@users.noreply.github.com>
2022-10-25 13:46:27 +0300

    Add files via upload

---
+++ projects/blog_app/precache-manifest.90264c305192383bef87fef69ab13816.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ed9a422f6ec846a2f25",
+    "url": "/projects/blog_app/static/css/main.e03a193b.chunk.css"
+  },
+  {
+    "revision": "1ed9a422f6ec846a2f25",
+    "url": "/projects/blog_app/static/js/main.1ed9a422.chunk.js"
+  },
+  {
+    "revision": "c939b1299540e7eb51d2",
+    "url": "/projects/blog_app/static/js/1.c939b129.chunk.js"
+  },
+  {
+    "revision": "b2cfa636897a7fbf6d27",
+    "url": "/projects/blog_app/static/js/runtime~main.b2cfa636.js"
+  },
+  {
+    "revision": "81e4bc02a2304ebd554fe3e00055c84a",
+    "url": "/projects/blog_app/index.html"
+  }
+];
---
+++ projects/got_app/precache-manifest.869da8377573f3cead7b49300c99b475.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3d4ae0ba85725d15e40",
+    "url": "/projects/got_app/static/css/main.2ed353e5.chunk.css"
+  },
+  {
+    "revision": "a3d4ae0ba85725d15e40",
+    "url": "/projects/got_app/static/js/main.a3d4ae0b.chunk.js"
+  },
+  {
+    "revision": "6382310aa219879c31d7",
+    "url": "/projects/got_app/static/css/1.f0436ef8.chunk.css"
+  },
+  {
+    "revision": "6382310aa219879c31d7",
+    "url": "/projects/got_app/static/js/1.6382310a.chunk.js"
+  },
+  {
+    "revision": "c4e35e62b047eed1d0dc",
+    "url": "/projects/got_app/static/js/runtime~main.c4e35e62.js"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/projects/got_app/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "6a72587deb3900bd2129eec70cd303e4",
+    "url": "/projects/got_app/index.html"
+  }
+];
