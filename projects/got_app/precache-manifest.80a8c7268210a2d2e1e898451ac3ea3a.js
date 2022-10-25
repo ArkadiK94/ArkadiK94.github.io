e6a596339203cab6fa4ea565805a2c916c1e166f
Arkadi Koifman <76536506+ArkadiK94@users.noreply.github.com>
2022-10-25 20:11:12 +0300

    Add files via upload

---
+++ projects/got_app/precache-manifest.80a8c7268210a2d2e1e898451ac3ea3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f274f3b0e734e3f251bc",
+    "url": "/projects/got_app/static/css/main.53988a93.chunk.css"
+  },
+  {
+    "revision": "f274f3b0e734e3f251bc",
+    "url": "/projects/got_app/static/js/main.f274f3b0.chunk.js"
+  },
+  {
+    "revision": "188f616e04321e2e9bed",
+    "url": "/projects/got_app/static/css/1.f0436ef8.chunk.css"
+  },
+  {
+    "revision": "188f616e04321e2e9bed",
+    "url": "/projects/got_app/static/js/1.188f616e.chunk.js"
+  },
+  {
+    "revision": "c4e35e62b047eed1d0dc",
+    "url": "/projects/got_app/static/js/runtime~main.c4e35e62.js"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/projects/got_app/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "e379df2ca92cdf1bac152990ba7b8656",
+    "url": "/projects/got_app/index.html"
+  }
+];
